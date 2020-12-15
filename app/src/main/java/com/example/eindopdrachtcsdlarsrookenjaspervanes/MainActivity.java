55dc58f8bf107c5be06e931dc519639c234107fc
Jasper <j.vanes1@student.avans.nl>
2020-12-15 10:49:33 +0100

    [ADD] added the map to the project

--- app/src/main/java/com/example/eindopdrachtcsdlarsrookenjaspervanes/MainActivity.java
+++ app/src/main/java/com/example/eindopdrachtcsdlarsrookenjaspervanes/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.eindopdrachtcsdlarsrookenjaspervanes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 import com.example.eindopdrachtcsdlarsrookenjaspervanes.fragments.MapFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
 //                    .commitNow();
 //        }
 
-
+        fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.findFragmentById(R.id.main_fragment) == null) {
+            mapFragment = new MapFragment();
+            fragmentManager.beginTransaction().add(R.id.main_fragment, mapFragment).commit();
+        } else {
+            mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.main_fragment);
+        }
     }
 }
--- app/src/main/java/com/example/eindopdrachtcsdlarsrookenjaspervanes/fragments/MapFragment.java
+++ app/src/main/java/com/example/eindopdrachtcsdlarsrookenjaspervanes/fragments/MapFragment.java
@@ -1,13 +1,23 @@
 package com.example.eindopdrachtcsdlarsrookenjaspervanes.fragments;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +25,23 @@ import android.view.ViewGroup;
 import com.example.eindopdrachtcsdlarsrookenjaspervanes.R;
 import com.example.eindopdrachtcsdlarsrookenjaspervanes.viewModels.MapViewModel;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
 
 public class MapFragment extends Fragment {
 
     private MapViewModel mViewModel;
+
+    private Context fragmentContext;
+    private IMapController mapController;
+    private MapView mapView;
+
+    private Marker currentLocation;
 
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -28,14 +51,59 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        fragmentContext = getActivity().getBaseContext();
+        Configuration.getInstance().load(fragmentContext, PreferenceManager.getDefaultSharedPreferences(fragmentContext));
+
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        mapView = view.findViewById(R.id.mapView);
+        mapView.setUseDataConnection(true);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+
+        mapController = mapView.getController();
+        mapController.setZoom(14);
+
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.getActivity(), new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 44);
+        }
+
+        mapView.setMultiTouchControls(true);
+        mapView.setBuiltInZoomControls(true);
+
+        return view;
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(MapViewModel.class);
-        // TODO: Use the ViewModel
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getLocation();
     }
+
+    public void getLocation() {
+        LocationManager locationManager = (LocationManager) fragmentContext.getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.d("Latitude", "onLocationChanged: " + location.getLatitude());
+                GeoPoint point = new GeoPoint(location.getLatitude(), location.getLongitude());
+                mapController.setCenter(point);
+                Marker startPoint = new Marker(mapView);
+                startPoint.setPosition(point);
+                mapView.getOverlays().remove(currentLocation);
+                currentLocation = startPoint;
+                mapView.getOverlays().add(startPoint);
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(fragmentContext, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 100, locationListener);
+        }
+    }
+
 
 }
